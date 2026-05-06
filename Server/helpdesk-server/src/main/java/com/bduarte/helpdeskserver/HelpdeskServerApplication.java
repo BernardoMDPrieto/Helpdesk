6ebe8013185c9a631f6dfb6e9dce0dc26e27f23f
Bernardo Prieto <beprieto47@gmail.com>
2026-05-05 21:21:11 -0300

    build: Estrutura inicial do backend com dependências iniciais para o projeto

---
+++ Server/helpdesk-server/src/main/java/com/bduarte/helpdeskserver/HelpdeskServerApplication.java
@@ -0,0 +1,13 @@
+package com.bduarte.helpdeskserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelpdeskServerApplication.class, args);
+    }
+
+}
